c7d89f9125e44e5ba5c66df5e8c013d951358c80
yonshii <dkaranets2@gmail.com>
2024-12-04 21:03:04 +0100

    All project files added

---
+++ src/app/DataHandler.java
@@ -0,0 +1,19 @@
+package app;
+
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+public class DataHandler {
+
+    private final Lock lock = new ReentrantLock();
+
+    public synchronized int modify(int num) {
+        lock.lock();
+        try {
+            num = num * 3;
+            return num;
+        } finally {
+            lock.unlock();
+        }
+    }
+}
